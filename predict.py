6afa81d15b73584594aa066eb23fdb1a3b90b2c5
Andris Vāravs <andris.varavs@lmt.lv>
2025-11-12 09:00:15 +0200

    predict.py: log timings

--- predict.py
+++ predict.py
@@ -1,4 +1,5 @@
 import io
+import time
 
 import requests
 import supervision as sv
@@ -15,14 +16,30 @@ from rfdetr import (
 from rfdetr.util.coco_classes import COCO_CLASSES
 
 # This also downloads model, if it's not present in working directory of script.
-model = RFDETRMedium()
 
+start_time = time.time()
+model = RFDETRMedium()
+model_load_time = time.time() - start_time
+print(f"Model loading time: {model_load_time:.4f} seconds")
+
+start_time = time.time()
 model.optimize_for_inference()
+optimize_time = time.time() - start_time
+print(f"Model optimization time: {optimize_time:.4f} seconds")
 
 # url = "https://media.roboflow.com/notebooks/examples/dog-2.jpeg"
 # image = Image.open(io.BytesIO(requests.get(url).content))
 image = Image.open("sample.jpg")
-detections = model.predict(image, threshold=0.5)
+warmup = 10
+iters = 10
+start_time = None
+for i in range(warmup + iters):
+    if i > warmup and start_time is None:
+        start_time = time.time()
+    detections = model.predict(image, threshold=0.5)
+
+predict_time = (time.time() - start_time) / iters
+print(f"Avg model prediction time: {predict_time:.4f} seconds")
 
 labels = [
     f"{COCO_CLASSES[class_id]} {confidence:.2f}"
